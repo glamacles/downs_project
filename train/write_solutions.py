130196de7e964c42704b9807cc25d85314dd2da5
Jake Downs <jacob.downs@umontana.edu>
2024-06-05 12:40:03 -0600

    into notebook

--- train/write_solutions.py
+++ train/write_solutions.py
@@ -7,18 +7,15 @@ from model import SpecFO
 Generate randomized synthetic ice sheets and solve for velocity.
 """
 
-#data = np.load('project/solution_data/data.npy')
+model = SpecFO()
 
-nx=75
-ny=75
-dx=125e3
-dy=125e3
-
-model = SpecFO(nx, ny, dx, dy)
-
+# Generate 3000 examples
 N = 3000
+# Max elevation ranges from 1500 - 4000 m
 S0 = np.random.uniform(1500., 4000., N)
+# Random length scale parameter for the bed
 sigmas = np.random.uniform(5., 15., N)
+# This randomizes the location of the ice sheet in the domain
 mid_offsets = 5000.*np.random.randn(N,2)
 
 plot = False
@@ -27,15 +24,17 @@ data = []
 for i in range(N):
     print(i)
 
+    # Generate random ice sheet geometry
     B, H = model.get_geometry(
         B0 = 1000.,
         S0 = S0[i],
         sigma=sigmas[i],
         mid_offset=mid_offsets[i]
     )
+
+    # Solve for velocity
     model.set_field(model.B, B)
     model.set_field(model.H, H)
-
     model.solver.solve()
 
     ubar0 = model.get_field(model.U.sub(0))
@@ -43,11 +42,13 @@ for i in range(N):
     udef0 = model.get_field(model.U.sub(2))
     udef1 = model.get_field(model.U.sub(3))
 
+    # Save inputs / outputs
     X = np.stack([
         B, H, ubar0, ubar1, udef0, udef1
     ])
     data.append(X)
 
+    # Optionally plot
     if plot:
         plt.subplot(3,2,1)
         plt.imshow(B)
@@ -75,8 +76,10 @@ for i in range(N):
 
         plt.show()
 
+    # Reset velocity so velocity solver converges
     model.U.assign(model.U*0.)
 
+# Save all the training data
 data = np.array(data)
 print(data.shape)
 np.save('data.npy', data)
